e735de1671e0fb47a3c9c45776697c0ee2cab7e1
James Howe <James.Howe@sds.co.uk>
2019-08-15 10:23:54 +0100

    Added menu items to menu component

--- app/wsc-menu/containers/wsc-menu/wsc-menu.component.ts
+++ app/wsc-menu/containers/wsc-menu/wsc-menu.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+class MenuItem {
+
+  text: string;
+
+  url: string;
+
+  constructor(text: string, url: string) {
+    this.text = text;
+    this.url = url;
+  }
+}
 
 @Component({
   selector: 'wsc-menu',
   styleUrls: ['wsc-menu.component.scss'],
   template: `
       <div>
-          wsc-menu
+          <ul>
+              <li *ngFor="let menu of menus">
+                  <a href="{{ menu.url }}">{{ menu.text }}</a>
+              </li>
+          </ul>
       </div>
   `
 })
-export class WscMenuComponent {
+export class WscMenuComponent implements OnInit {
+
+  menus: MenuItem[] = [];
+
+  ngOnInit(): void {
+    this.menus.push(new MenuItem("Home", "#"));
+    this.menus.push(new MenuItem("Help", "#"));
+  }
 
 }
